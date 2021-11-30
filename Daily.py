015704236856841d14cfcd197a765306c3a6a6cd
nctu0513325 <65332776+nctu0513325@users.noreply.github.com>
2021-11-30 16:04:40 +0800

    Update Daily.py

--- Daily.py
+++ Daily.py
@@ -5,14 +5,15 @@ import pandas as pd
 import re, os
 
 class Daily():
-    """Get daily stock data."""
+    """Get daily stock data, and list all stock which 本益比 < 15"""
     
     def __init__(self, start, end, gap):
         self.start = start
         self.end = end
         self.gap = gap
         self.daily_data = []
-        self.all_company = []
+        self.all_company = [] #each item is a list stored company 
+        self.all_company_code = [] #each item is a list stored company code
         
     def date_trans(self) -> list : 
         """Trans time into list to avoid wrong date."""
@@ -57,19 +58,33 @@ class Daily():
             pass
         for date in date_list:
             try:
-                r = requests.post(f'https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date={date}&type=ALL')
+                r = requests.get(f'https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date={date}&type=ALL')
+                # r = requests.post(f'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date={date}&selectType=ALL')
+                # info = r.text.split("\r")
+                # print(f'info:\n{info[1]}')
                 df = pd.read_csv(StringIO(r.text.replace("=", "")), 
                             header=["證券代號" in l for l in r.text.split("\n")].index(True)-1)
                 df = df.apply(lambda s: pd.to_numeric(s.astype(str).str.replace(",", "").replace("+", "1").replace("-", "-1"), errors = 'ignore'))
+                df.to_csv(f'daily_data/{date}.csv', encoding = 'utf_8_sig')
                 df = df[df['本益比'] < 15 ]
+                df = df[df['殖利率(%)'] > 4]
+                df = df[df['股價淨值比'] < 2]
+                
                 tmp = []
                 for com_name in df['證券名稱']:
                     tmp.append(com_name)
                 self.all_company.append(tmp)
+                
+                tmp = []
+                for com_code in df['證券代號']:
+                    tmp.append(com_code)
+                self.all_company_code.append(tmp)
+                
                 df.to_csv(f'daily_data/{date}.csv', encoding = 'utf_8_sig')
             except ValueError:
                 print(f'{date} is holiday, no data.')
         self.candi_company = list(set(self.all_company[0]).intersection(*self.all_company[1:]))
+        self.candi_company_code = list(set(self.all_company_code[0]).intersection(*self.all_company_code[1:]))
         print(len(self.candi_company))
         print(self.candi_company)
                 
