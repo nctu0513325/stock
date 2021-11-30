782e4dfe61dcf9c96e71cd8d3d94bed2c06ed8ab
nctu0513325 <sun1827sun@gmail.com>
2021-11-30 18:15:35 +0800

    sync up

--- Daily.py
+++ Daily.py
@@ -5,14 +5,15 @@ import pandas as pd
 import re, os
 
 class Daily():
-    """Get daily stock data."""
+    """Get daily stock data, and list all stock which 本益比 < 15"""
     
     def __init__(self, start, end, gap):
         self.start = start
         self.end = end
         self.gap = gap
         self.daily_data = []
-        self.all_company = []
+        self.all_company = [] #each item is a list stored company 
+        self.all_company_code = [] #each item is a list stored company code
         
     def date_trans(self) -> list : 
         """Trans time into list to avoid wrong date."""
@@ -57,19 +58,41 @@ class Daily():
             pass
         for date in date_list:
             try:
-                r = requests.post(f'https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date={date}&type=ALL')
-                df = pd.read_csv(StringIO(r.text.replace("=", "")), 
-                            header=["證券代號" in l for l in r.text.split("\n")].index(True)-1)
-                df = df.apply(lambda s: pd.to_numeric(s.astype(str).str.replace(",", "").replace("+", "1").replace("-", "-1"), errors = 'ignore'))
-                df = df[df['本益比'] < 15 ]
+                # r = requests.get(f'https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date={date}&type=ALL')
+                # df = pd.read_csv(StringIO(r.text.replace("=", "")), 
+                #             header=["證券代號" in l for l in r.text.split("\n")].index(True)-1)
+                # df = df.apply(lambda s: pd.to_numeric(s.astype(str).str.replace(",", "").replace("+", "1").replace("-", "-1"), errors = 'ignore'))
+                
+                # get data from website
+                requests.adapters.DEFAULT_RETRIES = 5
+                my_headers = {'user-agent': 'Chrome/45.0.2454.101'}
+                r = requests.get(f'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=20210201&selectType=ALL', headers = my_headers)
+                info = r.text.split("\r\n")
+                info = [l.replace('\"','').split(",")[:-1] for l in info[1:-13]]
+                info_df = pd.DataFrame(info[1:])
+                info[0].append('')
+                info_df.columns=info[0]
+                
+                # set stock choosing requirement
+                info_df = info_df[pd.to_numeric(info_df['本益比']) < 15 ]
+                info_df = info_df[pd.to_numeric(info_df['殖利率(%)']) > 4]
+                info_df = info_df[pd.to_numeric(info_df['股價淨值比']) < 2]
+                
                 tmp = []
-                for com_name in df['證券名稱']:
+                for com_name in info_df['證券名稱']:
                     tmp.append(com_name)
                 self.all_company.append(tmp)
-                df.to_csv(f'daily_data/{date}.csv', encoding = 'utf_8_sig')
+                
+                tmp = []
+                for com_code in info_df['證券代號']:
+                    tmp.append(com_code)
+                self.all_company_code.append(tmp)
+                
+                info_df.to_csv(f'daily_data/{date}.csv', encoding = 'utf_8_sig')
             except ValueError:
                 print(f'{date} is holiday, no data.')
         self.candi_company = list(set(self.all_company[0]).intersection(*self.all_company[1:]))
+        self.candi_company_code = list(set(self.all_company_code[0]).intersection(*self.all_company_code[1:]))
         print(len(self.candi_company))
         print(self.candi_company)
                 
