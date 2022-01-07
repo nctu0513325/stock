d77599fd97c24d111722016c10a46cc77076ef74
nctu0513325 <65332776+nctu0513325@users.noreply.github.com>
2022-01-07 17:27:01 +0800

    Update GA_distribute.py

--- GA_distribute.py
+++ GA_distribute.py
@@ -4,7 +4,23 @@ import numpy as np
 import csv
 import time
 import matplotlib.pyplot as plt
+from Daily import Sel_Company
 
-class GA_cal:
-    def __init__(self) -> None:
+# ============== parameter setting ===============
+NUM_CHROME = 100            
+Pc = 0.5    				# 交配率 (代表共執行Pc*NUM_CHROME/2次交配)
+Pm = 0.5   					# 突變率 (代表共要執行Pm*NUM_CHROME*Num_of_Job次突變)
+pressure = 0.1              # N-tourment 參數
+best_iteration = []                         #紀錄多快達到已知最佳解
+first = 0
+
+NUM_PARENT = NUM_CHROME                         # 父母的個數
+Num_pressure = int(pressure * NUM_CHROME)       
+NUM_CROSSOVER = int(Pc * NUM_CHROME / 2)        # 交配的次數
+NUM_CROSSOVER_2 = NUM_CROSSOVER*2               # 上數的兩倍
+NUM_MUTATION = int(Pm * NUM_CHROME * Num_of_Job)   # 突變的次數
+np.random.seed(0)
+
+def GA_main(candi_company_dic):
+    print("1")
         
