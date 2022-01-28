251f2d75c0104f5333f538bba133deb0466e4627
nctu0513325 <65332776+nctu0513325@users.noreply.github.com>
2022-01-28 16:48:10 +0800

    Update backtesting.py

--- backtesting.py
+++ backtesting.py
@@ -5,9 +5,10 @@ import re
 import datetime
 import requests
 import pandas as pd
+from collections import defaultdict
 
 def get_stock_price(stock, date):
-    r = requests.get(f'https://www.twse.com.tw/exchangeReport/STOCK_DAY_AVG?response=csv&date={date}&stockNo={stock}' )#,headers=my_headers)
+    r = requests.get(f'https://www.twse.com.tw/exchangeReport/STOCK_DAY_AVG?response=csv&date={date}&stockNo={stock}')#,headers=my_headers)
     info = [l[:-1].replace('\"','').replace("-",'-1').replace("+",'1').split(",") for l in r.text.split("\r\n")[1:-13]]
     info_dict = {z[0] : list(z[1:]) for z in zip(*info)}
     info_df = pd.DataFrame(info_dict)
@@ -31,19 +32,37 @@ def backtesting_main(startdate, end_date, reselect_gap = 1):
     for date in gen_backtesting_date_list(startdate, end_date, reselect_gap):
         # date = [start_date_for_selection, end_date_for_selection, buy_stock_day]
         
-        money_today = money_cash
-        for stock in stock_buy.keys():
-            money_today += get_stock_price(stock, date[2]) * int(stock_buy[stock])
-            
-        stock_code_new = Select(date[0], date[1])
-        stock_buy_new = GA_main(stock_code_new, date[0], date[1], money_cash)
-        
+        # test if buy_stock_day is on weekend to avoid no closing price
         time_tmp = re.search(r'(\d\d\d\d)(\d\d\d\d)', str(date[2]))
-        
         while isweekend(date[2]) or time_tmp.group(2) == '0101' :
             tmp = datetime.datetime.strptime(str(date[2]), '%Y%m%d')
             tmp += datetime.timedelta(days=1)
-            date[2] = int(datetime.datetime.strftime('%Y%m%d'))
+            date[2] = int(datetime.datetime.strftime(tmp, '%Y%m%d'))
+            time_tmp = re.search(r'(\d\d\d\d)(\d\d\d\d)', str(date[2]))
         
+        while isweekend(date[0]):
+            date[0] += 1
+            
+        # calculate money change
+        money_today = money_cash
+        for stock in stock_buy.keys():
+            money_today += get_stock_price(stock, date[2]) * int(stock_buy[stock])
+        
+        # select stock and decide how to buy
+        stock_code_new = Select(date[0], date[1])
+        stock_buy_new = GA_main(stock_code_new, date[0], date[1], money_today)       
+        
+        # calculate new share of each stock 
+        stock_buy_tmp = defaultdict(int)
+        for stock in stock_buy_new.keys():
+            stock_close_price = get_stock_price(stock, date[2])
+            money_on_stock = float(stock_buy_new[stock]) * money_today
+            share_of_stock = int(money_on_stock/stock_close_price)
+            stock_buy_tmp[stock] = share_of_stock
+            money_today -= share_of_stock * stock_close_price
+        
+        money_cash = money_today
+                    
+        print(f'done:{money_cash}')
 if __name__ == '__main__':
     backtesting_main(20210101, 20211231, 1)
